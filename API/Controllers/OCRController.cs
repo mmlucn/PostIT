e45a2636c20c06019671d2bf3041db2035b96d79
lilvacuum <93257374+lilvacuum@users.noreply.github.com>
2023-03-28 12:52:59 +0200

    h

---
+++ API/Controllers/OCRController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Tesseract;
+
+namespace PostIT_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OCRController : ControllerBase
+    {
+        TesseractEngine tesseractEngine;
+        public OCRController()
+        {
+          tesseractEngine = new TesseractEngine(@"./tessdata", "dan", EngineMode.Default);
+        }
+
+        [HttpPost]
+        public IActionResult Post(byte[] bytes)
+        {
+            using(var image = Pix.LoadFromMemory(bytes))
+            {
+                using(var page = tesseractEngine.Process(image))
+                {
+                    var text = page.GetText();
+                    return Ok(text);
+                }
+            }
+
+        }
+
+    }
+}
---
+++ API/Controllers/PostIT2Controller.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PostIT_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostIT2Controller : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get(int id)
+        {
+            return Ok(new { id });
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+        [HttpPost]
+        public IActionResult Post() {
+        
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult Put()
+        {
+            return Ok();
+        }
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            return Ok();
+        }
+
+    }
+}
