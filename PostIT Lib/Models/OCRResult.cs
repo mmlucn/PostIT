f804d67eee341c11ca8c122f6eebff736ee8707b
lilvacuum <93257374+lilvacuum@users.noreply.github.com>
2023-03-29 12:52:00 +0200

    LLL

---
+++ PostIT Lib/Models/OCRResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostIT_Lib.Models
+{
+    internal class OCRResult
+    {
+    }
+}
---
+++ PostIT Lib/Models/PostItNote.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostIT_Lib.Models
+{
+    internal class PostItNote
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public string Title { get; set; }
+        public int UserId { get; set; }
+        public DateTime Created { get; set; }
+        public string Category { get; set; }
+
+        
+
+
+
+    }
+}
